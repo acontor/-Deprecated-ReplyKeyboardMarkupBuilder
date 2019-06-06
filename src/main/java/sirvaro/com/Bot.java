e742582ba2a96afb156a76bc7869f7e180a7ff37
iamvincentliu <robin@robinsyl.dev>
2019-06-06 03:28:28 +0200

    Builders for ReplyKeyboard and InlineKeyboard

--- src/main/java/sirvaro/com/Bot.java
+++ src/main/java/sirvaro/com/Bot.java
@@ -1,7 +1,11 @@
 package sirvaro.com;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -15,15 +19,15 @@ public class Bot extends TelegramLongPollingBot {
             long chat_id = update.getMessage().getChatId();
             if (message_text.equals("/start")) {
                 SendMessage message = new SendMessage(chat_id, "App Menu:")
-                        .setReplyMarkup(ReplyKeyboardMarkupBuilder.create()
+                        .setReplyMarkup(ReplyKeyboardBuilder.createReply()
                                 .row()
-                                .addOption("Option 1")
-                                .addOption("Option 2")
+                                .addText("Option 1")
+                                .addText("Option 2")
                                 .row()
-                                .addOption("Option 3")
-                                .addOption("Option 4")
+                                .addText("Option 3")
+                                .addText("Option 4")
                                 .row()
-                                .addOption("Exit")
+                                .addText("Exit")
                                 .build()
                         );
                 try {
@@ -33,15 +37,33 @@ public class Bot extends TelegramLongPollingBot {
                 }
             } else if (message_text.equals("/settings")) {
                 SendMessage message = new SendMessage(chat_id, "Settings Menu:")
-                        .setReplyMarkup(ReplyKeyboardMarkupBuilder.create()
+                        .setReplyMarkup(ReplyKeyboardBuilder.createReply()
                                 .row()
-                                .addOption("Setting 1")
-                                .addOption("Setting 2")
+                                .addText("Setting 1")
+                                .addText("Setting 2")
                                 .row()
-                                .addOption("Setting 3")
-                                .addOption("Setting 4")
+                                .addText("Setting 3")
+                                .addText("Setting 4")
                                 .row()
-                                .addOption("Exit")
+                                .addText("Exit")
+                                .build()
+                        );
+                try {
+                    sendApiMethod(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (message_text.equals("/inline")) {
+                SendMessage message = new SendMessage(chat_id, "Inline Menu:")
+                        .setReplyMarkup(ReplyKeyboardBuilder.createInline()
+                                .row()
+                                .addCallbackData("Button 1", "btn 1")
+                                .addCallbackData("Button 2", "btn 2")
+                                .row()
+                                .addCallbackData("Button 3", "btn 3")
+                                .addCallbackData("Button 4", "btn 4")
+                                .row()
+                                .addCallbackData("Exit", "exit")
                                 .build()
                         );
                 try {
@@ -52,47 +74,12 @@ public class Bot extends TelegramLongPollingBot {
             } else {
 
                 // Example
-                SendMessage message = new SendMessage();
-                switch (message_text) {
-                    case "Option 1":
-                        message.setText("You have selected Option 1.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Option 2":
-                        message.setText("You have selected Option 2.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Option 3":
-                        message.setText("You have selected Option 3.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Option 4":
-                        message.setText("You have selected Option 4.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Setting 1":
-                        message.setText("You have selected Setting 1.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Setting 2":
-                        message.setText("You have selected Setting 2.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Setting 3":
-                        message.setText("You have selected Setting 3.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Setting 4":
-                        message.setText("You have selected Setting 4.");
-                        message.setChatId(chat_id);
-                        break;
-                    case "Exit":
-                        ReplyKeyboardRemove keyboard = new ReplyKeyboardRemove();
-                        keyboard.getRemoveKeyboard();
-                        message.setReplyMarkup(keyboard);
-                        message.setText("Bye");
-                        message.setChatId(chat_id);
-                        break;
+                SendMessage message = new SendMessage(chat_id, "You have selected " + message_text + ".");
+                if(message_text.equals("Exit")) {
+                    ReplyKeyboardRemove keyboard = new ReplyKeyboardRemove();
+                    keyboard.getRemoveKeyboard();
+                    message.setReplyMarkup(keyboard);
+                    message.setText("Bye");
                 }
                 try {
                     sendApiMethod(message);
@@ -101,6 +88,27 @@ public class Bot extends TelegramLongPollingBot {
                 }
             }
 
+        } else if (update.hasCallbackQuery()) {
+            CallbackQuery query = update.getCallbackQuery();
+            AnswerCallbackQuery answer = new AnswerCallbackQuery()
+                    .setCallbackQueryId(query.getId());
+            if (query.getData() != null) {
+                answer.setText(query.getData());
+            } else if (query.getGameShortName() != null) {
+                answer.setText(query.getGameShortName());
+            } else {
+                answer.setText("Button has no text");
+            }
+            if (query.getData().equals("exit")) {
+                EditMessageText edit = new EditMessageText()
+                        .setChatId(query.getMessage().getChatId())
+                        .setMessageId(query.getMessage().getMessageId());
+                try {
+                    sendApiMethod(edit);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
